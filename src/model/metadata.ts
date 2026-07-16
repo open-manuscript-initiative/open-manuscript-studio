d84368b4d06def89bf5132adbee60c62382bfffa
Varga István <115572434+vargaijanos@users.noreply.github.com>
2026-07-16 23:09:00 +0200

    Update metadata.ts

--- src/model/metadata.ts
+++ src/model/metadata.ts
@@ -1,1 +1,23 @@
+export interface LocalizedText {
+  default: string;
+  values: Record<string, string>;
+}
 
+export interface LocalizedKeyword {
+  id: string;
+  values: Record<string, string>;
+}
+
+export interface ManuscriptMetadata {
+  title: LocalizedText;
+  subtitle?: LocalizedText;
+  abstract?: LocalizedText;
+
+  language: string;
+  originalLanguage?: string;
+
+  keywords: LocalizedKeyword[];
+
+  discipline: string;
+  documentType: string;
+}
