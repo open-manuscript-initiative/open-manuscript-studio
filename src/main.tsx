4fd3f7c26fb0caba289716c0331ec1245bda1a81
Varga István <115572434+vargaijanos@users.noreply.github.com>
2026-07-21 16:19:32 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { I18nProvider } from './i18n';
 import './styles/global.css';
+import './styles/editor.css';
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
