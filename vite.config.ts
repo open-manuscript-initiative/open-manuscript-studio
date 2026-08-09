fc552327e5f35a2cc706e207f76381feff2d0f0e
Varga István <115572434+vargaijanos@users.noreply.github.com>
2026-08-09 18:22:53 +0200

    Make Vite Tauri-aware

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const tauriDevHost = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
   plugins: [react()],
+  clearScreen: false,
   server: {
     port: 5173,
-    host: true
-  }
+    strictPort: true,
+    host: tauriDevHost || true,
+    hmr: tauriDevHost
+      ? {
+          protocol: 'ws',
+          host: tauriDevHost,
+          port: 5174,
+        }
+      : undefined,
+  },
 });
