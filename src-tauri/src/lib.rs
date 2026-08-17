414acb981588dbac98d0df52e59486a92ba0ecb8
Varga István <115572434+vargaijanos@users.noreply.github.com>
2026-08-17 23:59:53 +0200

    Register updater commands

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,8 +1,19 @@
+#[cfg(desktop)]
+mod updater;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let builder = tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_fs::init());
+
+    #[cfg(desktop)]
+    let builder = builder.invoke_handler(tauri::generate_handler![
+        updater::check_for_update,
+        updater::install_update,
+    ]);
+
+    builder
         .run(tauri::generate_context!())
         .expect("error while running Open Manuscript Studio");
 }
