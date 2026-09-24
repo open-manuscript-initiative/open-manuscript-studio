6dbcbe009dfe3e2680d5d4ef4a7ba1be42440f6d
Varga István <vargawebkiado@gmail.com>
2026-09-24 08:30:18 +0200

    feat: register personal reference library routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -37,6 +37,7 @@ import { oidcProviderRouter } from './routes/oidcProviderRoutes.js';
 import { orcidLinkStartRouter } from './routes/orcidLinkStartRoutes.js';
 import { orcidOidcRouter } from './routes/orcidOidcRoutes.js';
 import { pdfImportRouter } from './routes/pdfImportRoutes.js';
+import { personalReferenceLibraryRouter } from './routes/personalReferenceLibraryRoutes.js';
 import {
   peerReviewRouter,
   peerReviewV1Router,
@@ -116,6 +117,7 @@ app.use('/api', cloudOAuthRouter);
 app.use('/api', cloudRouter);
 app.use('/api', userIntegrationRouter);
 app.use('/api', referenceManagerRouter);
+app.use('/api', personalReferenceLibraryRouter);
 app.use('/api', directSubmissionRouter);
 app.use('/api', agentReviewRouter);
 app.use('/api', integrationExecutionRouter);
